3e315eca005605a3ede22aa36df73b5b60fb411a
LuisHerranz <luisherranz@gmail.com>
2013-05-12 18:46:02 +0200

    Added the functions as template

--- lunaTestSuite.lua
+++ lunaTestSuite.lua
@@ -1,8 +1,35 @@
 module(..., package.seeall)
 
+------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------
+
+-- Use this function to add things that need to be done before running the whole suite
+function suite_setup()
+
+end
+
+-- Use this function to add things that need to be done before each single test
+function setup()
+
+end
+
+-- Use this function to add things that need to be done after each single test
+function teardown()
+	
+end
+
+-- Use this function to add things that need to be done after the whole suite
+function suite_teardown()
+
+end
+
+------------------------------------------------------------------------------------
 
 function test_assertTrue()
-	assert_true(false, "Just asserting true")
+	assert_true(true, "Just asserting true")
+	assert_equal(true, false, "Assert Equal")
+
 end
 
 function test_assertNil()
